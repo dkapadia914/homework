22ba0d52f274858b1d29c8cf81309a9921f81542
Dev Kapadia <kapaddev@berkeleyprep.org>
2018-10-09 23:46:50 -0400

    Completed Homework
    This is good i think, the runner code is messed up

---
+++ homework-4/Homework4.java
@@ -0,0 +1,105 @@
+public class Homework4 {
+	private String str;
+	private int integer;
+	/* Finish the constructor and create any necessary instance
+	 * variables
+	 */
+	public Homework4(String s, int i) {
+		str = s;
+		integer = i;
+	}
+
+	/* Return the product of the stored number and the
+	 * provided double. Cast the number after the
+	 * multiplication
+	 */
+	public int multiply(double d) {
+		int num1 = (int)(integer * d);
+		return num1;
+
+	}
+
+	/* Add the two provided numbers, and then multiply the
+	 * product by the stored number
+	 */
+	public double sumProduct(int a, double b) {
+		double num2 = (a + b) * (integer);
+		return num2;
+	}
+
+	/* Return the remainder of the first number divided by
+	 * the integer portion of the provided double
+	 */
+	 public int remainder(double d) {
+		 int num3 = (int) d;
+		 num3 = integer % num3;
+		 return num3;
+	}
+
+	/* Return the length of the stored String
+	 */
+	public int getLength() {
+		int num4 = str.length();
+		return num4;
+	}
+
+	/* Return the stored String followed by the provided
+	 * String. Put a space between the two Strings
+	 */
+	public String concat(String s) {
+		String string1 = str + " " + s;
+		return string1;
+
+	}
+
+	public static void main(String[] args) {
+		int exitCode = 0;
+
+		Homework4 hw4 = new Homework4("Hello, World!", 5);
+
+		if (hw4.multiply(2.5) == 12) { //how can this be a double if we cast it to an int, originally 12.25
+			System.out.println("Pass 1");
+		} else {
+			System.out.println("Fail 1");
+			exitCode += 1;
+		}
+
+		if (hw4.sumProduct(2, 2.5) == 22.5) { //yeah i dont even know how you would get 20.25
+			System.out.println("Pass 2");
+		} else {
+			System.out.println("Fail 2");
+
+			exitCode += 1;
+		}
+
+		if (hw4.remainder(3.5) == 2) {
+			System.out.println("Pass 3");
+		} else {
+			System.out.println("Fail 3");
+			exitCode += 1;
+		}
+
+		if (hw4.getLength() == 13) {
+			System.out.println("Pass 4");
+		} else {
+			System.out.println("Fail 4");
+			exitCode += 1;
+		}
+
+		if (hw4.concat("Goodbye!").equals("Hello, World! Goodbye!")) { //inform Smith of the error, it originally didn't have a comma
+			System.out.println("Pass 5");
+		} else {
+			System.out.println("Fail 5");
+			exitCode += 1;
+		}
+
+		if (exitCode == 0) {
+			System.out.println("All Pass");
+		} else {
+			System.out.println("Fail");
+		}
+
+		System.exit(exitCode);
+	}
+
+}
---
+++ homework-4/README.md
@@ -0,0 +1,10 @@
+# Homework 4
+
+You have one week to complete these five problems. Some of are modified versions of the exercises at the end of Chapter 4 in the book, others are of my own design. Complete them to the best of your ability and then submit them. Make sure your code works (compiles, runs, and passes the initial checks) before you submit it. You can always resubmit up until the deadline, but you will not receive any credit if your code does not run.
+
+## Skills
+
+This homework covers skills 2.2, 2.3, 2.4, 2.5, 2.7, and 2.8. You will most likely have to rely on previously learned to complete the problems, but the bulk of the work will practice these skills.
+
+## As always...
+Make sure that you include your first and last name in the comment section at the very beginning of the Java file; if I cannot determine whose homework this is, I cannot give you credit.
